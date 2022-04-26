239eed35d8ae50095849af7e0ea215f3c3345e1c
Night6997 <83875915+Night6997@users.noreply.github.com>
2022-04-26 22:51:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 song1="";
 song2="";
 
-leftWristX="";
-leftWristY="";
-rightWristX="";
-rightWristY="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+leftWrist_Score=0;
+rightWrist_Score=0;
 
 function preload(){
 
     song1=loadSound("music1.mp3");
     song2=loadSound("music2.mp3");
+    song1_Status=song1.isPlaying()
+    song2_Status=song2.isPlaying();
 
 }
 
@@ -46,6 +50,11 @@ function gotPoses(results){
         rightWristY=results[0].pose.rightWrist.y;
         console.log("Right wrist x = "+rightWristX+", right wrist y = "+rightWristY);
 
+        leftWrist_Score=results[0].pose.keypoints[9].score;
+        rightWrist_Score=results[0].pose.keypoints[10].score;
+        console.log("Left wrist score = "+leftWrist_Score+", right wrist score = "+rightWrist_Score);
+
+
     }
 
 }
@@ -53,5 +62,37 @@ function gotPoses(results){
 function draw(){
 
     image(video,0,0,600,500);
+    if(leftWrist_Score>0.2){
+
+        fill("#20d6be");
+        stroke("#20d6be");
+        circle(leftWristX,leftWristY,20);
+
+        song.stop();
+        if(song1_Status=="false"){
+
+            song.play(song1);
+            document.getElementById("songName_btn").innerHTML="Song playing now is song 1";
+            console.log("MUSIC IS PLAYING");
+
+        }
+
+    }
+    if(rightWrist_Score>0.2){
+
+        fill("#20d6be");
+        stroke("#20d6be");
+        circle(rightWristX,rightWristY,20);
+
+        song.stop();
+        if(song2_Status=="false"){
+
+            song.play(song2);
+            document.getElementById("songName_btn").innerHTML="Song playing now is song 2";
+            console.log("MUSIC IS PLAYING");
+
+        }
+
+    }
 
 }
